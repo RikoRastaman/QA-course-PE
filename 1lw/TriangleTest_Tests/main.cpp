cf3c51b1749497f1c9ee6bb9914a5f35330cfaa2
Maks Semeev <makssemeev@gmail.com>
2019-10-15 00:03:17 +0300

    add read from file

--- 1lw/TriangleTest_Tests/main.cpp
+++ 1lw/TriangleTest_Tests/main.cpp
@@ -1,22 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include <array>
 #include <stdio.h>
-#include <stdlib.h>
+#include "windows.h"
+#include <string>
 
-int main() {
-	/*string cmd = "..\..\TriangleTest\x64\Release\TriangleTest.exe";
-	int out1 = system("cmd");
-	return out1;*/
-	setlocale(LC_ALL, "Russian");
 
+std::string getOutput(std::string& arguments) {
 	std::string result;
-	std::string cmd = "TriangleTest.exe 3 4 6";
+	std::string cmd = "TriangleTest.exe " + arguments;
 	std::array<char, 256> buffer;
-	FILE * _pipe;
+	FILE* _pipe;
 	_pipe = _popen(cmd.c_str(), "r");
-	
+
 	fgets(buffer.data(), 256, _pipe);
 	result += buffer.data();
+	return result;
+}
 
-	std::cout << result << std::endl;
+int main(int argc, char* argv[]) {
+	setlocale(LC_ALL, "Russian");
+	std::string expectedOutput = "";
+	if (argc != 2) {
+		std::cout << "Enter test file name!";
+		return 0;
+	}
+	std::fstream input(argv[1]);
+	std::ofstream output;
+	std::string arguments;
+	int counter = 0;
+	while (getline(input, arguments)) {
+		counter++;
+		getline(input, expectedOutput);
+		std::cout << "Test" << counter << ": " + expectedOutput + " = " + getOutput(arguments) << std::endl;
+		if (expectedOutput == getOutput(arguments)) {
+			std::cout << "Passed" << std::endl;
+		}
+		else {
+			std::cout << "Failed" << std::endl;
+		}
+
+	}
 }
