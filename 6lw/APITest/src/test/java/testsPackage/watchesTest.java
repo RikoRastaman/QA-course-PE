54de0f39404e301292c5ad697bb0149dff218a57
Maks Semeev <makssemeev@gmail.com>
2019-12-07 00:18:16 +0300

    Update watchesTest.java

--- 6lw/APITest/src/test/java/testsPackage/watchesTest.java
+++ 6lw/APITest/src/test/java/testsPackage/watchesTest.java
@@ -7,6 +7,6 @@ public class watchesTest {
 
     @Test
     public void check_googleToUp() {
-        given().when().get("http://www.google.com").then().statusCode(200);
+        given().when().get("http://52.136.215.164:9000/api/products").then().statusCode(200);
     }
 }
