6d89df18763fb24a1b7fc9984ce6a2cc0e456490
Maks Semeev <makssemeev@gmail.com>
2019-12-08 02:03:56 +0300

    constants for POST and GET added, description added

--- 6lw/APITest/src/test/java/testsPackage/watchesTest.java
+++ 6lw/APITest/src/test/java/testsPackage/watchesTest.java
@@ -1,41 +1,48 @@
 package testsPackage;
 
-import com.sun.xml.internal.bind.v2.model.core.ID;
 import io.restassured.http.ContentType;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
+
 public class watchesTest {
 
+    static String GET_ALL_PRODUCTS = "http://52.136.215.164:9000/api/products";
+    static String GET_DELETE_BY_ID = "http://52.136.215.164:9000/api/deleteproduct?id=";
+    static String POST_ADD_PRODUCT = "http://52.136.215.164:9000/api/addproduct";
+    static String POST_EDIT_PRODUCT = "http://52.136.215.164:9000/api/editproduct";
+
     @Test
-    public void test_ResponseHeaderData_ShouldBeCorrect() {
+    public void GetAllProducts_ResponseHeaderAndStatusCode_ShouldBeCorrect() {
         given().
         when().
-            get("http://52.136.215.164:9000/api/products").
+            get(GET_ALL_PRODUCTS).
         then().assertThat().statusCode(200).
             and().
             contentType(ContentType.JSON).
         and().
             header("Content-Type", equalTo("application/json"));
+        System.out.println("All products response header correct, status 200");
     }
 
     @Test
-    public void test_APIWithBasicAuthentication_ShouldBeGivenAccess() {
+    public void BasicAuthenticationToAddProduct_ShouldBeGivenAccess() {
 
         given().
             auth().
             preemptive().
             basic("test177", "test177").
         when().
-            get("http://52.136.215.164:9000/api/addproduct").
+            get(POST_ADD_PRODUCT).
         then().
             assertThat().
             statusCode(200);
+        System.out.println("Authentication to add access given, status 200");
     }
 
     @Test
-    public void test_APIDeleteProduct_ProductDeleted() {
+    public void APIDeleteProduct_ProductDeleted() {
         String IDtoDelete = "403";
         given().
             param("id", IDtoDelete).
@@ -43,9 +50,10 @@ public class watchesTest {
             preemptive().
             basic("test177", "test177").
         when().
-            get("http://52.136.215.164:9000/api/deleteproduct").
+            get(GET_DELETE_BY_ID).
         then().
             assertThat().
             statusCode(200);
     }
+
 }
