ae47b3c80ad779726fa31177a000c5c5a80948a1
Maks Semeev <makssemeev@gmail.com>
2019-12-08 01:17:52 +0300

    API delete product test added

--- 6lw/APITest/src/test/java/testsPackage/watchesTest.java
+++ 6lw/APITest/src/test/java/testsPackage/watchesTest.java
@@ -1,12 +1,51 @@
 package testsPackage;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import io.restassured.http.ContentType;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
 public class watchesTest {
 
     @Test
-    public void check_googleToUp() {
-        given().when().get("http://52.136.215.164:9000/api/products").then().statusCode(200);
+    public void test_ResponseHeaderData_ShouldBeCorrect() {
+        given().
+        when().
+            get("http://52.136.215.164:9000/api/products").
+        then().assertThat().statusCode(200).
+            and().
+            contentType(ContentType.JSON).
+        and().
+            header("Content-Type", equalTo("application/json"));
+    }
+
+    @Test
+    public void test_APIWithBasicAuthentication_ShouldBeGivenAccess() {
+
+        given().
+            auth().
+            preemptive().
+            basic("test177", "test177").
+        when().
+            get("http://52.136.215.164:9000/api/addproduct").
+        then().
+            assertThat().
+            statusCode(200);
+    }
+
+    @Test
+    public void test_APIDeleteProduct_ProductDeleted() {
+        String IDtoDelete = "403";
+        given().
+            param("id", IDtoDelete).
+            auth().
+            preemptive().
+            basic("test177", "test177").
+        when().
+            get("http://52.136.215.164:9000/api/deleteproduct").
+        then().
+            assertThat().
+            statusCode(200);
     }
 }
