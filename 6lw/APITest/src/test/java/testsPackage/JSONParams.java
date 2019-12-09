fd10f9f4ad864aa8df4aa3c71b722bd0282babc1
Maks Semeev <makssemeev@gmail.com>
2019-12-10 02:23:20 +0300

    Wrote more tests for POST

--- 6lw/APITest/src/test/java/testsPackage/JSONParams.java
+++ 6lw/APITest/src/test/java/testsPackage/JSONParams.java
@@ -3,10 +3,9 @@ package testsPackage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-public class  JSONParams {
-
-    public static JSONObject Params() {
-        JSONObject obj = new JSONObject();
+class JSONParams {
+    private JSONObject obj = new JSONObject();
+    JSONParams() {
         obj.put("category_id", 1);
         obj.put("title", "test 777");
         obj.put("content", "definitely content");
@@ -16,6 +15,23 @@ public class  JSONParams {
         obj.put("keywords", "777");
         obj.put("description", "huge text");
         obj.put("hit", 1);
+    }
+
+    void ChangeValue(String keyToChange, String valueToChange) {
+        obj.remove(keyToChange);
+        obj.put(keyToChange, valueToChange);
+    }
+
+    void PutArrayInKeys() {
+        obj.put("keywords", "key1, key2");
+    }
+
+    void ChangeValue(String keyToChange, int valueToChange) {
+        obj.remove(keyToChange);
+        obj.put(keyToChange, valueToChange);
+    }
+
+    JSONObject GetParams() {
         return obj;
     }
 }
--- 6lw/APITest/src/test/java/testsPackage/watchesTest.java
+++ 6lw/APITest/src/test/java/testsPackage/watchesTest.java
@@ -2,24 +2,29 @@ package testsPackage;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ResponseBodyExtractionOptions;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static io.restassured.RestAssured.given;
-import static org.junit.Assert.assertEquals;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class watchesTest {
 
     static String GET_ALL_PRODUCTS = "http://52.136.215.164:9000/api/products";
     static String GET_DELETE_BY_ID = "http://52.136.215.164:9000/api/deleteproduct";
     static String POST_ADD_PRODUCT = "http://52.136.215.164:9000/api/addproduct";
     static String POST_EDIT_PRODUCT = "http://52.136.215.164:9000/api/editproduct";
+    static int ID_OF_NOT_TEST_PRODUCT = 269;
     static String LOGIN = "test177";
     static String PASSWORD = "test177";
+    public int firstTestID = 591;
+    public int lastTestID = 602;
 
     @Test
-    public void GetAllProducts_ResponseHeaderAndStatusCode_ShouldBeCorrect() {
+    public void test1_GetAllProducts_ResponseHeaderAndStatusCode_ShouldBeCorrect() {
         given().
                 when().
                 get(GET_ALL_PRODUCTS).
@@ -32,8 +37,7 @@ public class watchesTest {
     }
 
     @Test
-    public void BasicAuthenticationToAddProduct_ShouldBeGivenAccess() {
-
+    public void test2_BasicAuthenticationToAddProduct_ShouldBeGivenAccess() {
         given().
                 auth().
                 preemptive().
@@ -42,63 +46,254 @@ public class watchesTest {
                 get(POST_ADD_PRODUCT).
                 then().
                 assertThat().
-                statusCode(200);
+                statusCode(200).body("status", equalTo(0));
         System.out.println("Authentication to add access given, status 200");
     }
 
     @Test
-    public void APIDeleteProduct_ProductDeleted() {
-        String IDtoDelete = "507";
-        given().
-                param("id", IDtoDelete).
-                auth().
-                preemptive().
-                basic(LOGIN, PASSWORD).
-                when().
-                get(GET_DELETE_BY_ID).
-                then().
-                assertThat().
-                statusCode(200);
-    }
+    public void test3_POSTProductWithAllValidParams_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
 
-    @Test
-    public void POSTProduct_WithJSONParams() {
-        RestAssured.baseURI = "http://52.136.215.164:9000/api/addproduct";
-        given().
+        ResponseBodyExtractionOptions body = given().
                 contentType("application/json").
-                body(JSONParams.Params().toString()).
-                when().
-                post("").then();
-    }
-
-    @Test
-    public void POSTProduct_WithJSONParams_GetResponseStatus_StatusIs1() {
-        RestAssured.baseURI = "http://52.136.215.164:9000/api/addproduct";
-        int status = given().
-                contentType("application/json").
-                body(JSONParams.Params().toString()).
+                body(params.GetParams().toString()).
                 when().
                 post("").
                 then().
-                assertThat().
-                statusCode(200).extract().path("status");
-        assertEquals(1, status);
+                body("status", equalTo(1))
+                .extract().response();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with valid params added, with ID:" + IDProduct);
     }
 
     @Test
-    public void DeleteAllCreatedTests() {
-        String IDtoDelete = "507";
-        for (int IDtoDelete2 = 505; IDtoDelete2 < 511; IDtoDelete2++) {
-            given().
-                    param("id", IDtoDelete2).
-                    auth().
-                    preemptive().
-                    basic(LOGIN, PASSWORD).
-                    when().
-                    get(GET_DELETE_BY_ID).
-                    then().
-                    assertThat().
-                    statusCode(200);
+    public void test4_POSTProductWithRussianTitle_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("title", "тест на русском 177");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with russian title added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test5_POSTProductWithEmptyTitle_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("title", "");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with empty title added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test6_POSTProductWithCategoryOutOfRange_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("category", "16");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with category out of range added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test7_POSTProductWithNegativeCategory_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("category", "-2");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with negative category added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test8_POSTProductWithNegativePrice_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("price", "-100");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with negative price added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test9_POSTProductWithZeroPrice_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("price", "0");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with zero price added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test10_POSTProductWithMAXPrice_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("price", Integer.MAX_VALUE + 1);
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with MAX price added, overflow of int, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test11_POSTProductWithOldPriceLeastThenActualPrice_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("price", "999");
+        params.ChangeValue("old_price", "777");
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product with old price least then actual added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test12_POSTProductWithStatus0_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("status", 0);
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product With Status 0 added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test13_POSTProductWithHit0_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+        params.ChangeValue("hit", 0);
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product With Hit 0 added, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test14_POSTProductWithKeyWords_GetResponseStatus_StatusIs1() {
+        RestAssured.baseURI = POST_ADD_PRODUCT;
+        JSONParams params = new JSONParams();
+
+        params.ChangeValue("title", "IHaveKeys");
+        params.PutArrayInKeys();
+
+        ResponseBodyExtractionOptions body = given().
+                contentType("application/json").
+                body(params.GetParams().toString()).
+                when().
+                post("").
+                then().
+                body("status", equalTo(1)).
+                extract().body();
+
+        int IDProduct = body.path("id");
+        System.out.println("Product With keys, with ID:" + IDProduct);
+    }
+
+    @Test
+    public void test99_DeleteAllCreatedTests() {
+        if (firstTestID > ID_OF_NOT_TEST_PRODUCT) {
+            for (int IDtoDelete = firstTestID; IDtoDelete <= lastTestID; IDtoDelete++)
+                given().
+                        param("id", IDtoDelete).
+                        auth().
+                        preemptive().
+                        basic(LOGIN, PASSWORD).
+                        when().
+                        get(GET_DELETE_BY_ID).
+                        then().
+                        assertThat().
+                        statusCode(200);
         }
     }
 }
